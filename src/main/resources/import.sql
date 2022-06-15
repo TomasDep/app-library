103e51a7fad9f3fecd5c648224a790c84c400ae1
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-15 18:58:26 -0400

    [CHG] Creado datos de prueba para los usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+/*------------------------------------- EJEMPLOS CLIENTES -------------------------------------*/
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('john', 'doe', 'john@gmail.com', 'Calle Leonardo da Vinci, 7, 41092', '9 5555 2222', NOW())
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('jane', 'doe', 'jane@gmail.com', 'Calle de Velázquez, 80 Madrid', '9 8888 2222', NOW())
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('richard', 'lee', 'rlee@gmail.com', 'Calle 12 entre 51 y 53', '9 0222 2635', NOW())
@@ -8,3 +9,16 @@ INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('H
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('Amal', 'Merrill', 'cursus.et@outlook.org', 'Ap #948-7662 Eu Road', '9 5494 6762', NOW())
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('Callum', 'Ferguson', 'nibh@google.net', '377-6338 Orci Rd.', '9 3746 6831', NOW())
 INSERT INTO clients (name, lastname, email, address, phone, create_at) VALUES('Isaac', 'Leonard', 'auctor@aol.couk', '5211 Tempus, Street', '9 1180 8266', NOW())
+
+/*------------------------------------- EJEMPLOS USUARIOS -------------------------------------*/
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Ashton', 'Miller', 'ametMassa', 'amet.massa@aol.org', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Patience', 'Hull', 'etMagnis', 'et.magnis@hotmail.net', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Demetrius', 'Bryan', 'tellusSuspendisseSed', 'tellus.suspendisse.sed@icloud.ca', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Jason', 'Preston', 'morbiSitAmet', 'morbi.sit.amet@outlook.com', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Josiah', 'Ryan', 'tellusSem', 'tellus.sem@aol.couk', '12345', '1', NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Harding', 'Marshall', 'sedMalesuada', 'sed.malesuada@protonmail.couk', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Mikayla', 'Cherry', 'etMalesuadaFames', 'et.malesuada.fames@outlook.com', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Robert', 'Tyler', 'cursusEt', 'cursus.et@aol.com', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('August', 'Kirby', 'sagittisSemperNam', 'sagittis.semper.nam@yahoo.org', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Bell', 'Witt', 'administrador', 'admin@email.com', '12345', true, NOW());
+INSERT INTO users (name, lastname, username, email, password, enabled, create_at) VALUES('Adrienne', 'Pollard', 'usuario', 'user@email.com', '12345', true, NOW());
