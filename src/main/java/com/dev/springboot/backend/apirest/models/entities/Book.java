b36130d330e738b71ad23f5eb0d6b3971e81a5a7
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-18 20:44:50 -0400

    [ADD] Se crea entidad de Libro

---
+++ src/main/java/com/dev/springboot/backend/apirest/models/entities/Book.java
@@ -0,0 +1,54 @@
+package com.dev.springboot.backend.apirest.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+public class Book implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String name;
+
+    @NotEmpty
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String status;
+
+    private String observation;
+
+    private Integer stock;
+
+    @NotNull
+    private Double price;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createAt;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void PrePersist() {
+        this.createAt = new Date();
+    }
+}
