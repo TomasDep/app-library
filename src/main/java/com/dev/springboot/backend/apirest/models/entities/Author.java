c04badafc59ee1f13657648d229bcce5f73e8f40
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-18 20:44:25 -0400

    [ADD] Se crea entidad autor

---
+++ src/main/java/com/dev/springboot/backend/apirest/models/entities/Author.java
@@ -0,0 +1,43 @@
+package com.dev.springboot.backend.apirest.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+public class Author implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    @Column(nullable = false)
+    private String name;
+    @NotEmpty
+    @Column(nullable = false)
+    private String lastname;
+    private String country;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private List<Book> book;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createAt;
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @PrePersist
+    public void PrePersist() {
+        this.createAt = new Date();
+    }
+}
