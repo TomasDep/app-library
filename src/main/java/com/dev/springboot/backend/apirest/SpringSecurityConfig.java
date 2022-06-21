73d7aa1c685fb50fac4ae139dc2f9cd0682185ab
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-21 15:25:38 -0400

    [CHG] Se agrega ruta publica

--- src/main/java/com/dev/springboot/backend/apirest/SpringSecurityConfig.java
+++ src/main/java/com/dev/springboot/backend/apirest/SpringSecurityConfig.java
@@ -57,7 +57,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**", "/api/genres").permitAll()
+                .antMatchers("/api/auth/**", "/api/genres", "/api/authors").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(this.jwtEntryPoint)
