c6d14b4b3ada656fee8700ec55394c7bc7c97208
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-27 17:56:53 -0400

    [CHG] Se agrega roles

--- src/main/java/com/dev/springboot/backend/apirest/dto/UserDto.java
+++ src/main/java/com/dev/springboot/backend/apirest/dto/UserDto.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -18,9 +20,12 @@ public class UserDto {
     @NotBlank
     private String username;
 
+    @NotBlank
     @Email
     private String email;
 
     @NotBlank
     private String password;
+
+    private Set<String> roles = new HashSet<>();
 }
