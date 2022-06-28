9b01feda4277be9686e47e869f3c71429f339de6
TomasDep <tomas.valenzuelacb@gmail.com>
2022-06-28 18:13:56 -0400

    [CHG] Se cambia metodo findAll

--- src/main/java/com/dev/springboot/backend/apirest/models/services/IClientService.java
+++ src/main/java/com/dev/springboot/backend/apirest/models/services/IClientService.java
@@ -1,11 +1,10 @@
 package com.dev.springboot.backend.apirest.models.services;
 
 import com.dev.springboot.backend.apirest.models.entities.Client;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface IClientService {
-    public List<Client> findAll();
+    public Page<Client> findAll(int pageNumber, int pageSize);
     public Client findById(Long id);
     public Client save(Client client);
     public void delete(Long id);
